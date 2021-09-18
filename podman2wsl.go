7af8eede799d85d0bfc322020a4601727b634059
Piotr Stapp <ptrstpp950@gmail.com>
2021-09-18 17:15:44 +0200

    A lot of work :)

--- podman2wsl.go
+++ podman2wsl.go
@@ -3,11 +3,13 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
 	"regexp"
 	"strings"
+	"time"
 )
 
 func convertArgument(argument string) string {
@@ -19,39 +21,53 @@ func convertArgument(argument string) string {
 		toReplace := "$1" + root + "/$3"
 		argument = pathRegexp.ReplaceAllString(argument, toReplace)
 		argument = slashRegexp.ReplaceAllString(argument, "/")
+		argument = strings.ReplaceAll(argument, ";", "\\;")
 	}
-	argument = strings.ReplaceAll(argument, ";", "\\;")
 	return argument
 }
 
 func convertArguments(args []string) []string {
 	for index := 0; index < len(args); index++ {
 		args[index] = convertArgument(args[index])
+		if args[index] == "{{json .}}" {
+			args[index] = "table {{json .}}"
+		}
+	}
+	if args[0] == "events" {
+		for index := 0; index < len(args); index++ {
+			if args[index] == "-f" {
+				args[index] = "--format"
+			}
+		}
 	}
 	return args
 }
 
 func main() {
-	mydir, _ := os.Getwd()
-	fmt.Println(mydir)
+	//CMD: podman system service -t 0 &
+	//mydir, _ := os.Getwd()
+	//fmt.Println(mydir)
+	callId := fmt.Sprintf("%d", time.Now().UnixNano())
 	args := os.Args[1:]
 	args = convertArguments(args)
 	args = append([]string{"podman"}, args...)
 	cmd := exec.Command("wsl.exe", args...)
-	/*file, _ := os.OpenFile("temp.log", os.O_APPEND|os.O_WRONLY, 0644)
+	file, _ := os.OpenFile("C:\\projects\\aspnet-core-test\\temp.log", os.O_APPEND|os.O_WRONLY, 0644)
+	log.SetOutput(file)
 	defer file.Close()
-	if _, err := file.WriteString(strings.Join(args, " ") + "\n"); err != nil {
-		log.Fatal(err)
-	}*/
-	var out bytes.Buffer
-	var stderr bytes.Buffer
-	cmd.Stdout = &out
-	cmd.Stderr = &stderr
+	log.Print("START callId: " + callId + " cmd: " + strings.Join(args, " "))
+	if len(args) >= 4 && strings.Contains(args[3], ".Server.Os") {
+		os.Stdout.WriteString("linux")
+		//log.Print("HACKED callId: " + callId + "\ncmd:\n" + strings.Join(args, "@"))
+		return
+	}
+	var stdoutBuf, stderrBuf bytes.Buffer
+	cmd.Stdout = io.MultiWriter(os.Stdout, &stdoutBuf)
+	cmd.Stderr = io.MultiWriter(os.Stderr, &stderrBuf)
 	err := cmd.Run()
 	if err != nil {
-		os.Stdin.Write(out.Bytes())
-		os.Stderr.Write(stderr.Bytes())
-		log.Fatal(err)
+		outStr, errStr := stdoutBuf.String(), stderrBuf.String()
+		log.Print("ERROR OUT callId: " + callId + "\ncmd:\n" + strings.Join(args, "@") + "\nout:\n" + outStr + "\nerr:\n" + errStr + "\n")
+		log.Fatalf("ERROR cmd.Run(%s) failed with %s\n", strings.Join(args, "@"), err)
 	}
-	os.Stdout.Write(out.Bytes())
 }
