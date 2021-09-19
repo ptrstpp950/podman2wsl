2f021f3e89bc3d5eb256c811e25fae0c6a930b28
piotr.stapp <ptrstpp950@gmail.com>
2021-09-19 21:33:41 +0200

    Missing test cases

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module stapp.space/podman2wsl
 
 go 1.16
+
+require github.com/stretchr/testify v1.7.0
--- podman2wsl_test.go
+++ podman2wsl_test.go
@@ -1,28 +1,71 @@
 package main
 
-import "testing"
+import (
+	"testing"
 
-func TestConvertArgument(t *testing.T) {
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_convertArgument_Should_Escape_Windows_Paths(t *testing.T) {
 	cases := []struct{ test, expected, caseName string }{
 		{"c\\a\\", "c\\a\\", "Not a path shouldn't be converted"},
 		{"c:\\a\\b\\c", "/mnt/c/a/b/c", "Convert to mnt"},
 		{"C:\\a\\B\\c", "/mnt/c/a/B/c", "Driver letter must be small in mnt"},
 		{"C:\\", "/mnt/c/", "Root should be converted"},
 		{"X:\\a\\", "/mnt/x/a/", "Other driver letter then C should be supported"},
-		{"string ; with ;;colons", "string \\; with \\;\\;colons", "All colons should be escaped"},
 	}
-	//expected := []string{"c\\a\\b", "/mnt/c/a/b/c", "/mnt/D/bbbbb", "-'/mnt/c/q'", ""}
-	//test := []string{"c\\a\\b", "c:\\a\\b\\c", "D:\\bbbbb", "-'c:\\q'", `"C:\projects\aspnet-core-test"`}
-	/*actual := convertPath(test)
-	for index := 0; index < len(test); index++ {
-		if actual[index] != expected[index] {
-			t.Error("Test failed:" + actual[index] + "!=" + expected[index])
-		}
-	}*/
 	for _, c := range cases {
 		actual := convertArgument(c.test)
-		if actual != c.expected {
-			t.Error("Test " + c.caseName + " failed:" + actual + "!=" + c.expected)
-		}
+		assert.Equal(t, c.expected, actual, c.caseName)
+	}
+}
+
+func Test_convertArguments_Should_Ignore_Host_Arg(t *testing.T) {
+	args := []string{"--host", "127.0.0.1", "last"}
+	result := convertArguments(args)
+	expected := []string{"last"}
+	assert.Equal(t, result, expected)
+}
+
+func Test_convertArguments_Should_Escape_Collon_In_Env(t *testing.T) {
+	expected := []string{"something", "-e", "AAA=x\\;y", "-v", "aaaa;bbb"}
+	args := []string{"something", "-e", "AAA=x;y", "-v", "aaaa;bbb"}
+	result := convertArguments(args)
+	assert.Equal(t, result, expected)
+}
+
+func Test_convertArguments_Should_Escape_Dollar_In_Last_Arg_In_exec(t *testing.T) {
+	expected := []string{"exec", "aaa$bbb", "aaa\\$bbb"}
+	args := []string{"exec", "aaa$bbb", "aaa$bbb"}
+	result := convertArguments(args)
+	assert.Equal(t, result, expected)
+}
+
+func Test_convertArguments_Should_Use_Full_Filter_Name_In_events(t *testing.T) {
+	expected := []string{"events", "--filter", "xxx"}
+	args := []string{"events", "-f", "xxx"}
+	result := convertArguments(args)
+	assert.Equal(t, result, expected)
+}
+
+func Test_convertArguments_Should_Replace_JSON_Format(t *testing.T) {
+	cases := []struct {
+		test, expected []string
+		caseName       string
+	}{
+		{
+			[]string{"ps", "{{json .}}"},
+			[]string{"ps", "{\"Command\":\"\\\"{{.Command}}\\\"\",\"CreatedAt\":\"{{.CreatedAt}}\",\"ID\":\"{{.ID}}\",\"Image\":\"{{.Image}}\",\"Labels\":\"{{.Labels}}\",\"LocalVolumes\":\"0\",\"Mounts\":\"{{.Mounts}}\",\"Names\":\"{{.Names}}\",\"Networks\":\"{{.Networks}}\",\"Ports\":\"{{.Ports}}\",\"RunningFor\":\"{{.RunningFor}}\",\"Size\":\"Unknown (TODO)\",\"State\":\"{{.State}}\",\"Status\":\"{{.Status}}\"}"},
+			"ps JSON format conversion failed",
+		},
+		{
+			[]string{"other", "{{json .}}"},
+			[]string{"other", "table {{json .}}"},
+			"JSON format conversion failed",
+		},
+	}
+	for _, c := range cases {
+		result := convertArguments(c.test)
+		assert.Equal(t, c.expected, result)
 	}
 }
